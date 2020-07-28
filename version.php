5ec0194f9b3d58905201186ce1b6c5292ef0cd37
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-07-28 10:37:59 +0200

    Release v1.3.1.zhaw0 / 2020072801

--- version.php
+++ version.php
@@ -21,8 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2020072800;
+$plugin->version = 2020072801;
 $plugin->requires  = 2018051700;         // minimum: Moodle 3.5
 $plugin->maturity = MATURITY_RC;
-$plugin->release = 'v1.3.1';
+$plugin->release = 'v1.3.1.zhaw0';
 $plugin->component = 'tool_asynccourseimport'; // Full name of the plugin (used for diagnostics).
